3668a788f78400417000071186d125dd7fcd07b1
Oliver Liu <spaghettarian+get@gmail.com>
2018-01-13 21:29:08 -0800

    Added index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,26 @@
-$(document).ready(() => {
-  $('#submit-url-btn').click(() => {
+function urlInputIsNotEmpty() {
+  return $('#url-input').val() !== '';
+}
+
+function onClickShortenBtn() {
+  $('#shorten-success-alert').hide();
+  if (urlInputIsNotEmpty()) {
     $.post(
       '/api/shorten',
       {
         url: $('#url-input').val(),
       },
       (res, status) => {
-        console.log(status);
-        console.log(res);
+        if (status === 'success') {
+          $('#ret-url').val(res.url);
+          $('#shorten-success-alert').show();
+        }
       },
     );
-  });
+  }
+}
+
+$(document).ready(() => {
+  new Clipboard('#copy-btn');
+  $('#shorten-btn').click(onClickShortenBtn);
 });
